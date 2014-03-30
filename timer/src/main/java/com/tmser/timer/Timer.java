e3841dcee67468c9112d61a9f069a84bbea01ebd
tjx222 <tjx1222@163.com>
2014-03-30 22:19:54 +0800

    创建定时器子项目

---
+++ timer/src/main/java/com/tmser/timer/Timer.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.tmser.timer;
+
+/**
+ * 定时器主程序
+ * @author tjx
+ * @version 2.0
+ * 2014-3-30
+ */
+public class Timer {
+	
+	public static void main(String[] args) {
+		java.util.Timer tm =  new java.util.Timer();
+	}
+
+}
